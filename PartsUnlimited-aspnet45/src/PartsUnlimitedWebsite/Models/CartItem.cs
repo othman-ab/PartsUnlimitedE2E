546ee4fb79e599002cf894b444edb748f52480c3
Othman Abid <othmanabid2001@gmail.com>
2023-04-04 14:54:13 +0000

    my first change

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //my first change.
+        //myy first change.
         [Key]
         public int CartItemId { get; set; }
 
